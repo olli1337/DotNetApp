e9d0c6583e138a027d60a1301225610c63325295
olli1337 <olli@havilehto.fi>
2021-11-02 22:48:59 +0200

    Add support for Get/List API Endpoint cancellation token (including debug code)

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -7,6 +7,7 @@ using Domain;
 using System.Threading;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Activities
 {
@@ -18,15 +19,28 @@ namespace Application.Activities
         {
 
             private readonly DataContext _context;
+        private readonly ILogger<List> _logger;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, ILogger<List> logger)
             {
+                _logger = logger;
                 _context = context;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.ToListAsync();
+
+                try {
+                    for (var i = 0; i < 10; i++) {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        _logger.LogInformation($"Task {i} has completed!");
+                    }
+                } catch (Exception ex) when (ex is TaskCanceledException) {
+                    _logger.LogInformation("Task was cancelled!");
+                }
+
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
