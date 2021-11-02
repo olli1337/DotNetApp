a3f6c333d34cc5be8c22d482745a1f38a427d9b2
olli1337 <olli@havilehto.fi>
2021-11-03 00:36:09 +0200

    Remove unnecessary txt file

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -31,11 +31,7 @@ namespace Application.Activities
             {
 
                 try {
-                    for (var i = 0; i < 10; i++) {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed!");
-                    }
+                    cancellationToken.ThrowIfCancellationRequested();                    
                 } catch (Exception ex) when (ex is TaskCanceledException) {
                     _logger.LogInformation("Task was cancelled!");
                 }
