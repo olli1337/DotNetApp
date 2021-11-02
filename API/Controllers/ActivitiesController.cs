e63c38bf61f8d19a738c1542525fafa3895ccdfa
olli1337 <olli@havilehto.fi>
2021-11-02 17:18:09 +0200

    Add placeholder implementation for PUT API endpoint

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -27,5 +27,11 @@ namespace API.Controllers
         public async Task<IActionResult> Createctivity(Activity activity) {
             return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity) {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
     }
 }
