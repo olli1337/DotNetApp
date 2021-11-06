1f2b3720d0867e3eda3ea9f68ed3505bfa8e7376
olli1337 <olli@havilehto.fi>
2021-11-06 15:42:17 +0200

    Fix form autoComplete typo

--- Client-App/src/features/activities/form/ActivityForm.tsx
+++ Client-App/src/features/activities/form/ActivityForm.tsx
@@ -33,7 +33,7 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
 
     return(
         <Segment clearing>
-            <Form onSubmit={handleSubmit} autocomplete='off' >
+            <Form onSubmit={handleSubmit} autoComplete='off' >
                 <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
                 <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
                 <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
