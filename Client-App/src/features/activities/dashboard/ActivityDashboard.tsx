8bd5f7cafbb0d9a8294f21a2353eea571aca5889
olli1337 <olli@havilehto.fi>
2021-11-06 12:39:58 +0200

    Add a component for visualizing activity details

--- Client-App/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Client-App/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,7 +1,9 @@
+import { groupEnd } from 'console';
 import React from 'react';
 import { Grid, List } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import Activitylist from './ActivityList';
+import ActivityDetails from './details/ActivityDetails';
 
 interface Props {
     activities: Activity[];
@@ -12,6 +14,11 @@ export default function ActivityDashboard({activities}: Props) {
         <Grid>
             <Grid.Column width='10'>
                 <Activitylist activities={activities} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {
+                    activities[0] && <ActivityDetails activity={activities[0]} /> 
+                }
             </Grid.Column>
         </Grid>
     )
